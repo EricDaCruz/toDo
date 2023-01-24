339bd4974d0743ed896299eeaad570d20a801cce
Eric Rafael <rafaeleric40@gmail.com>
2023-01-24 08:14:50 -0300

    :sparkles: feat: Populando o BD

---
+++ server/prisma/seed.ts
@@ -0,0 +1,76 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const firstHabitId = "0730ffac-d039-4194-9571-01aa2aa0efbd";
+const firstHabitCreationDate = new Date("2022-12-31T03:00:00.000");
+
+const secondHabitId = "00880d75-a933-4fef-94ab-e05744435297";
+const secondHabitCreationDate = new Date("2023-01-03T03:00:00.000");
+
+const thirdHabitId = "fa1a1bcf-3d87-4626-8c0d-d7fd1255ac00";
+const thirdHabitCreationDate = new Date("2023-01-08T03:00:00.000");
+
+async function run() {
+   await prisma.task.deleteMany();
+   await prisma.category.deleteMany();
+
+   /**
+    * Create Categories
+    **/
+   await Promise.all([
+      prisma.category.create({
+         data: {
+            name: "Escola",
+         },
+      }),
+
+      prisma.category.create({
+         data: {
+            name: "Trabalho",
+         },
+      }),
+
+      prisma.category.create({
+         data: {
+            name: "Programação",
+         },
+      }),
+   ]);
+
+   /**
+    * Create Tasks
+    **/
+   await Promise.all([
+      prisma.task.create({
+         data: {
+            title: "Lição de Geografia",
+            categoryId: 1,
+         },
+      }),
+
+      prisma.task.create({
+         data: {
+            title: "Lição de Matemática",
+            categoryId: 1,
+         },
+      }),
+
+      prisma.task.create({
+         data: {
+            title: "Estudar NodeJS",
+            categoryId: 3,
+         },
+      }),
+   ]);
+}
+
+run()
+   .then(async () => {
+      await prisma.$disconnect();
+   })
+   .catch(async (e) => {
+      console.error(e);
+      await prisma.$disconnect();
+      process.exit(1);
+   });
